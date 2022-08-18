9d773482ec329ef82baf08f6cc0aa27bd713a58c
Victor <mrethanlucas@gmail.com>
2022-08-18 20:20:00 +0100

    sec

--- scripts/interaction.ts
+++ scripts/interaction.ts
@@ -2,22 +2,19 @@ import { ethers } from "hardhat";
 
 const main = async () => {
 
-//   const stakingJuliet = await ethers.getContractAt("IStaking", "0x5C8f5D80FD653a885832dF5c1f09978C81A8cDC4");
+    const Staking = await ethers.getContractFactory("Staking");
+    const staking = Staking.attach("0xAb4155dad989DFa948cC1ea25438e6b9e3C2A6E7");
 
-    ////// Alternative way of interactng with a deployed contract without using Contract Interface /////
-    const StakingJuliet = await ethers.getContractFactory("Staking");
-    const stakingJuliet = StakingJuliet.attach("0xAb4155dad989DFa948cC1ea25438e6b9e3C2A6E7");
-
-  const stakeTxn = await stakingJuliet.stake(1, { value: ethers.utils.parseEther("0.1")});
-  const stakeTxnReciept = await stakeTxn.wait();
+  const stakeTxne = await staking.stake(1, { value: ethers.utils.parseEther("0.1")});
+  const stakeTxnReciept = await stakeTxne.wait();
   console.log("Stake: ", stakeTxnReciept);
 
-    const stakeBalanceTxn = await stakingJuliet.checkContractBalance();
+    const stakeBalanceTxn = await staking.checkContractBalance();
     console.log("Stake: ", stakeBalanceTxn);
 
-//  const stakeWithdrawTxn = await stakingJuliet.withdraw();
-//  const stakeWithdrawTxnReciept = await stakeWithdrawTxn.wait();
-//  console.log("Stake: ", stakeWithdrawTxnReciept);
+ const stakeWithdrawTxn = await staking.withdraw();
+ const stakeWithdrawTxnReciept = await stakeWithdrawTxn.wait();
+ console.log("Stake: ", stakeWithdrawTxnReciept);
   
 
 }
